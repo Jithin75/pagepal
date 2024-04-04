56585eb6ab05198f60891ee3c986b30787788049
Jithin75 <jithuk02@gmail.com>
2024-04-04 17:16:03 -0400

    Recommendation Loading Screen

--- PagePal/src/main/kotlin/view/RecommendationView.kt
+++ PagePal/src/main/kotlin/view/RecommendationView.kt
@@ -1,10 +1,7 @@
 package view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -12,10 +9,12 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mongodb.MongoClientSettings
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import kotlinx.coroutines.runBlocking
@@ -32,12 +31,15 @@ import org.json.JSONArray
 import org.json.JSONObject
 import theme.darkblue
 import theme.grey
+import theme.lightbrown
 import viewmodel.BookViewModel
 import viewmodel.RecommendationViewModel
 
 @Composable
 fun RecommendationView(mainPageViewModel: MainPageViewModel, recommendationViewModel: RecommendationViewModel) {
     var dbManager: DatabaseManager? by remember { mutableStateOf(null) }
+    var isLoading by remember { mutableStateOf(true) } // State for loading
+    var displayedBooks by remember { mutableStateOf<List<BookModel>>(emptyList()) }
 
     LaunchedEffect(Unit) {
         dbManager = runBlocking {
@@ -50,103 +52,159 @@ fun RecommendationView(mainPageViewModel: MainPageViewModel, recommendationViewM
             val database = client.getDatabase("PagePalDB").withCodecRegistry(pojoCodecRegistry)
             DatabaseManager(database)
         }
+        try {
+            var userBooks = mainPageViewModel.getUserLibrary()
+
+            val question = StringBuilder()
+            question.append("The user has read the following books: \\n")
+
+            userBooks.forEachIndexed { index, book ->
+                // Append the title and authors to the question string
+                question.append("${book.title}: ${book.author}")
+
+                // Append newline character except for the last book
+                if (index < userBooks.size - 1) {
+                    question.append("\\n")
+                }
+            }
+
+            displayedBooks = mutableListOf<BookModel>()
+            // Pass the constructed question string to the getResponse function
+            val response = AIRecommender.getResponse(question.toString(), 10)
+            val jsonObject = JSONObject(response)
+            val jsonArray: JSONArray = jsonObject.getJSONArray("recommendations")
+            val bookTitles = mutableListOf<String>()
+            for (i in 0 until jsonArray.length()) {
+                val bookObject = jsonArray.getJSONObject(i)
+                val title = bookObject.getString("title")
+                bookTitles.add(title)
+            }
+
+            val bookClient = BookApiClient()
+            val books = mutableListOf<BookModel>()
+
+            for (title in bookTitles) {
+                val book : BookModel = bookClient.searchBook(title)
+                val pattern = Regex("&zoom=\\d+")
+                val replacement = "&zoom=3"
+                val cover = book.cover
+                book.cover = cover.replace(pattern, replacement)
+                books.add(book)
+            }
+
+            displayedBooks = books // Update displayedBooks
+        } catch (e: Exception) {
+            println(e)
+        } finally {
+            isLoading = false // Hide loading screen when tasks complete
+        }
     }
 
     MaterialTheme {
-        Scaffold (
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(
-                            text = "PagePal",
-                            color = Color.White,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(end = 68.dp),
-                            textAlign = TextAlign.Center
-                        )
-                    },
-                    backgroundColor = grey,
-                    navigationIcon = {
-                        IconButton(onClick = {mainPageViewModel.toggleRecommendPage()}) {
-                            Icon(
-                                Icons.Filled.ArrowBack,
-                                contentDescription = "Back",
-                                tint = Color.White
+        // Show loading screen while data is being fetched
+        if (isLoading) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "PagePal",
+                                color = Color.White,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(end = 68.dp),
+                                textAlign = TextAlign.Center
                             )
-                        }
-                    }
-                )
-            },
-            content = {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = darkblue)
-                        .padding(10.dp)
-                ) {
-                    // Book Grid
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(5),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                    ) {
-                        var userBooks = mainPageViewModel.getUserLibrary()
-
-                        val question = StringBuilder()
-                        question.append("The user has read the following books: \\n")
-
-                        userBooks.forEachIndexed { index, book ->
-                            // Append the title and authors to the question string
-                            question.append("${book.title}: ${book.author}")
-
-                            // Append newline character except for the last book
-                            if (index < userBooks.size - 1) {
-                                question.append("\\n")
+                        },
+                        backgroundColor = grey,
+                        navigationIcon = {
+                            IconButton(onClick = { mainPageViewModel.toggleRecommendPage() }) {
+                                Icon(
+                                    Icons.Filled.ArrowBack,
+                                    contentDescription = "Back",
+                                    tint = Color.White
+                                )
                             }
                         }
-
-//                      var bookResponseString = ""
-//                      var displayedBooks by remember { mutableStateOf(mutableListOf<BookModel>()) }
-                        var displayedBooks = mutableListOf<BookModel>()
-                        // Pass the constructed question string to the getResponse function
-                        val response = AIRecommender.getResponse(question.toString(), 10)
-                        val jsonObject = JSONObject(response)
-                        val jsonArray: JSONArray = jsonObject.getJSONArray("recommendations")
-                        val bookTitles = mutableListOf<String>()
-                        for (i in 0 until jsonArray.length()) {
-                            val bookObject = jsonArray.getJSONObject(i)
-                            val title = bookObject.getString("title")
-                            bookTitles.add(title)
-                        }
-
-                        val bookClient = BookApiClient()
-
-                        for (title in bookTitles) {
-                            val book : BookModel = bookClient.searchBook(title)
-                            val pattern = Regex("&zoom=\\d+")
-                            val replacement = "&zoom=3"
-                            val cover = book.cover
-                            book.cover = cover.replace(pattern, replacement)
-                            displayedBooks.add(book)
-                        }
-
-                        // Update displayedBooks within the composable context
-                        val library = displayedBooks.toList()
-                        items(items = library, key = {it.cover}) { book ->
-                            BookItem(
-                                book,
-                                onClick = { recommendationViewModel.onBookClick(book) }
+                    )
+                }, content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = darkblue),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "LOADING...",
+                            color = lightbrown,
+                            fontSize = 30.sp
+                        )
+                    }
+                }
+            )
+        } else {
+            // Actual content of the screen
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "PagePal",
+                                color = Color.White,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(end = 68.dp),
+                                textAlign = TextAlign.Center
                             )
+                        },
+                        backgroundColor = grey,
+                        navigationIcon = {
+                            IconButton(onClick = { mainPageViewModel.toggleRecommendPage() }) {
+                                Icon(
+                                    Icons.Filled.ArrowBack,
+                                    contentDescription = "Back",
+                                    tint = Color.White
+                                )
+                            }
+                        }
+                    )
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = darkblue)
+                            .padding(10.dp)
+                    ) {
+                        // Book Grid
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(5),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+
+                            // Update displayedBooks within the composable context
+                            val library = displayedBooks.toList()
+                            items(items = library, key = { it.cover }) { book ->
+                                BookItem(
+                                    book,
+                                    onClick = { recommendationViewModel.onBookClick(book) }
+                                )
+                            }
                         }
                     }
                 }
+            )
+            if (recommendationViewModel.isBookOpen) {
+                val bookModel = recommendationViewModel.bookOpened ?: BookModel(title = "error")
+                BookView(
+                    mainPageViewModel,
+                    BookViewModel(bookModel, mainPageViewModel.dbManager),
+                    isRecommended = true,
+                    onDismiss = { recommendationViewModel.onDismissBook() })
             }
-        )
-        if(recommendationViewModel.isBookOpen) {
-            val bookModel = recommendationViewModel.bookOpened ?: BookModel(title = "error")
-            BookView(mainPageViewModel, BookViewModel(bookModel, mainPageViewModel.dbManager), isRecommended = true, onDismiss = {recommendationViewModel.onDismissBook()})
         }
     }
 }
